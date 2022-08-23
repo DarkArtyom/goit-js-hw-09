996c5897349c38aecaee36813e4cad9d5e134872
Artem Shnurko <shnurko.artyom@gmail.com>
2022-08-23 16:03:55 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let changeBG = null;
+
+function onChangeBG() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+startButton.addEventListener('click', () => {
+  changeBG = setInterval(() => {
+    onChangeBG();
+  }, 1000);
+  startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(changeBG);
+  startButton.disabled = false;
+});
